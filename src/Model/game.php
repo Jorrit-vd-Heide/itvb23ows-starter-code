ad11ceaf777b067e3a26c7f22a735fa54a83f387
Jorrit-vd-Heide <35562079+Jorrit-vd-Heide@users.noreply.github.com>
2024-02-05 12:15:48 +0100

    moved pass restart and undo to game

--- src/Model/game.php
+++ src/Model/game.php
@@ -30,4 +30,38 @@ class Game {
    public function __construct($database) {
        $this->database = $database;
    }
+
+   public function changePlayer(){
+       $this->activePlayer = 1 - $this->activePlayer;
+   }
+
+   public function pass(){
+        $stmt = $db->prepare('insert into moves
+        (game_id, type, move_from, move_to, previous_id, state)
+        values
+        (?, "pass", null, null, ?, ?)');
+        $stmt->bind_param('iis', $_SESSION['game_id'], $_SESSION['last_move'], setState());
+        $stmt->execute();
+        $_SESSION['last_move'] = $db->insert_id;
+        $_SESSION['player'] = 1 - $_SESSION['player'];
+   }
+
+   public function undo(){
+        $stmt = $db->prepare('SELECT * FROM moves WHERE id = '.$_SESSION['last_move']);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_array();
+        $this->last_move = $result[5];
+        getState($result[6]);
+   }
+
+   public function restart(){
+        $this->board = [];
+        $this->hand = [
+            0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3],
+            1 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]];
+        $this->activePlayer = 0;
+        $stmt = $this->database->prepare('INSERT INTO games VALUES ()');
+        $stmt->execute();
+        $this->game_id = $db->insert_id;
+   }
 }
