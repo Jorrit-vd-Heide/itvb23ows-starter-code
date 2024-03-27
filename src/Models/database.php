62cb22c0c281fac35146807d6ddd09bd478deafb
Jorrit-vd-Heide <35562079+Jorrit-vd-Heide@users.noreply.github.com>
2024-03-27 12:57:38 +0100

    Fixed database error

--- src/Models/database.php
+++ src/Models/database.php
@@ -8,7 +8,7 @@ function retrieveDatabase() {
     global $config;
     if ($database === null) {
         // If the database connection object is null, establish a new connection
-        $database = new mysqli($config['db_host'], $config['db_user'], $config['db_password'], $config['db_name']);
+        $database = new mysqli($config['db_host'], $config['db_user'], '', $config['db_name']);
     }
     // Return the database connection object
     return $database;
